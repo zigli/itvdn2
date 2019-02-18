fdfe6637a776e16f02b19c48073d4051903fc046
ilgiz <4ilgiz@gmail.com>
2019-02-18 15:57:13 +0300

    innerclass with static field

--- src/nested_classes/Main.java
+++ src/nested_classes/Main.java
@@ -1,9 +1,13 @@
 package nested_classes;
 
 class MyClass{
+    private static int field;
+
     public class Inner{
-        public void method(){
-            System.out.println("Method in Inner class");
+        public void method(int a){
+            field = a;
+
+            System.out.println("Method in Inner class, with field = " + field);
         }
     }
     public void method(){
@@ -15,7 +19,7 @@ class MyClass{
 public class Main {
     public static void main(String[] args) {
         MyClass.Inner instance = new MyClass().new Inner();
-        instance.method();
+        instance.method(2);
 
         MyClass instance1 = new MyClass();
         instance1.method();
