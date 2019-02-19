60bd66b160ae3dddf8924fce0af8e0b7624a8ec0
ilgiz <4ilgiz@gmail.com>
2019-02-19 14:03:26 +0300

    Static classes can have non-static methods. Static method can not be used in non-static classes

--- src/nested_classes4/Main.java
+++ src/nested_classes4/Main.java
@@ -2,6 +2,9 @@ package nested_classes4;
 
 class MyClass{
     public static class Nested{
+        void say(){
+            System.out.println("hello");
+        }
         public static void staticMethodFromNested(){
             System.out.println("Static Method From Nested class");
         }
@@ -11,5 +14,7 @@ class MyClass{
 public class Main {
     public static void main(String[] args) {
         MyClass.Nested.staticMethodFromNested();
+        MyClass.Nested instance = new MyClass.Nested();
+        instance.say();
     }
 }
